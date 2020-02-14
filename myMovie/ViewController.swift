527ead16563916f0451c49d491a6cb5bfa3853a6
carlsbug <keonwoong.min@sjsu.edu>
2020-02-13 22:38:16 -0800

    Initial Commit

---
+++ myMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myMovie
+//
+//  Created by Keon Min on 2/13/20.
+//  Copyright © 2020 kwmin605. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
